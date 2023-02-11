d12c69a69dfa66c99be3f02de3a7ad2e628898d5
mori <mori@liberty-innovation.co.jp>
2023-02-11 22:45:53 +0900

    なんとか形になったかもしれない

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-
 const container = document.querySelector(".container");
 container.addEventListener("scroll", () => {
-    const box_wrap = document.querySelector(".box_wrap");
-    scrollTop_value = container.scrollTop;
-    clientHeight = container.clientHeight;
-    console.log(`scroll top ${scrollTop_value} - ${clientHeight}`);
-    if (scrollTop_value > 100 && scrollTop_value < 110) {
-        console.log("in!!!");
-        box_wrap.innerHTML += `
-                                <div class="box"></div>
-    `;
-    }
+  const box_wrap = document.querySelector(".box_wrap");
+
+  scrollTop_value = container.scrollTop;
+  clientHeight = container.clientHeight;
+  console.log(`scroll top ${scrollTop_value} - ${clientHeight}`);
+
+  const box2 = document.querySelector(".box2");
+  const box3 = document.querySelector(".box3");
+
+  if (scrollTop_value < 10) {
+    box2.classList.remove("slideIn");
+    box3.classList.remove("slideIn");
+  }
+  if (scrollTop_value > 10) {
+    console.log("slideIn 1");
+    box2.classList.add("slideIn");
+  }
+  if (scrollTop_value > clientHeight) {
+    console.log("slideIn 2");
+    box3.classList.add("slideIn");
+  }
 });
