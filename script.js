94d4b8cbb2560d6f0d431dfc56a13c0d5a5fa34e
liberty-mori-toshiaki <mori@liberty-innovation.co.jp>
2023-02-11 17:34:21 +0900

    retry

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ container.addEventListener("scroll", () => {
     scrollTop_value = container.scrollTop;
     clientHeight = container.clientHeight;
     console.log(`scroll top ${scrollTop_value} - ${clientHeight}`);
-    if (scrollTop_value == 850) {
-        alert("in!!!");
+    if (scrollTop_value > 100 && scrollTop_value < 110) {
+        console.log("in!!!");
         box_wrap.innerHTML += `
                                 <div class="box"></div>
     `;
